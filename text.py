578fa94893f54c98a24870a92589b20a2349e0b0
Mono <monocode22@gmail.com>
2025-03-21 23:11:00 +0200

    Fixed sizes and multiple line text

--- text.py
+++ text.py
@@ -12,7 +12,6 @@ class Text():
         self._position_percent = position_percent
 
     def add_text_to_image(self, draw_ctx, image_size: tuple):
-        # TODO: add text wrap
         # TODO: add this module to images as well
 
         try:
@@ -20,9 +19,9 @@ class Text():
         except IOError:
             text_font = ImageFont.load_default()
 
-        # lines = textwrap.wrap(self._text, width=20)
-        # print(lines)
-        # for line in lines:
-        text_width = draw_ctx.textlength(self._text, font=text_font)
-        text_position = ((image_size[0] - text_width) * self._position_percent[0], image_size[1] * self._position_percent[1])
-        draw_ctx.text(text_position, self._text, fill=self._color, font=text_font)
+        lines = textwrap.wrap(self._text, width=25)
+        text_width = draw_ctx.textlength(lines[0], font=text_font)
+        fixed_position = ((image_size[0] - text_width) * self._position_percent[0], image_size[1] * self._position_percent[1])
+        for line in lines:
+            draw_ctx.text(fixed_position, line, fill=self._color, font=text_font)
+            fixed_position = (fixed_position[0], fixed_position[1] + self._size)
