b41e582f3f0bc56a576a9f877f024a38ec9d8ea4
Mono <monocode22@gmail.com>
2025-03-21 22:36:50 +0200

    Added text module to support multiple text boxes

--- card_generator.py
+++ card_generator.py
@@ -1,11 +1,21 @@
 from card import Card
+from text import Text
+
+TITLE_COLOR = (67, 27, 21)
 
 def main():
+    title_text = Text(
+        text="Bulbasaur",
+        font="Quasimodo",
+        color=TITLE_COLOR,
+        size=30,
+        position_percent=(0.5, 0.1)
+    )
+
     munchkin_card = Card(
         card_background="munchkin",
         card_title="Bulbasaur",
-        title_font="Quasimodo",
-        text_font="Quasimodo"
+        texts=[title_text]
     )
 
     munchkin_card.create_card()
