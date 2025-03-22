456090ce41642908e3cd515d3bb4c5529414a93c
Mono <monocode22@gmail.com>
2025-03-22 15:51:21 +0200

    Added pokemon munchkin card class

--- src/munchkin_card.py
+++ src/munchkin_card.py
@@ -4,13 +4,12 @@ from sprite import Sprite
 
 class MunchkinCard(Card):
     MUNCHKIN_CARD = "munchkin"
-    MUNCHKIN_POKEMON_TYPE = "types\\Pokemon_Type_Icon_{pokemon_type}"
 
     TITLE_FONT = "Quasimodo"
     BODY_FONT = "CaslonAntique"
     FONT_COLOR = (67, 27, 21)
 
-    def __init__(self, card_title: str, bonus: int, description_text: str, ally: int, coins: int, types: list):
+    def __init__(self, card_title: str, bonus: int, description_text: str, bottom_left_text: str, bottom_right_text: str, additional_sprites: list):
         texts = []
         texts.append(Text(
             text=f"+{bonus} Bonus",
@@ -37,7 +36,7 @@ class MunchkinCard(Card):
         ))
 
         texts.append(Text(
-            text=f"{ally} Ally",
+            text=bottom_left_text,
             font=self.BODY_FONT,
             color=self.FONT_COLOR,
             size=30,
@@ -45,33 +44,18 @@ class MunchkinCard(Card):
         ))
 
         texts.append(Text(
-            text=f"{coins} Coins",
+            text=bottom_right_text,
             font=self.BODY_FONT,
             color=self.FONT_COLOR,
             size=30,
             position_percent=(0.9, 0.9)
         ))
 
-        sprites = []
+        sprites = additional_sprites 
         sprites.append(Sprite(
             sprite=card_title,
             size=(425, 425),
             position_percent=(0.5, 0.1)
         ))
         
-        # TODO: create pokemon munchkin class
-        if len(types) == 1:
-            x_position_percent = 0.5
-            offset = 0
-        else:
-            x_position_percent = 0.4
-            offset = 0.2
-        for pokemon_type in types:
-            sprites.append(Sprite(
-                sprite=self.MUNCHKIN_POKEMON_TYPE.format(pokemon_type=pokemon_type),
-                size=(50, 50),
-                position_percent=(x_position_percent, 0.885)
-            ))
-            x_position_percent += offset
-
         super().__init__(card_background=self.MUNCHKIN_CARD, output_name=card_title, texts=texts, sprites=sprites)
